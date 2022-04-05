a63b4a5aad48a3d210f2c89cf8f5f3dccde55d78
KYHSGeekCode <jourhyang123@gmail.com>
2022-04-05 14:18:11 +0900

    :zap: Better performance

--- music_type_classifier/svm.py
+++ music_type_classifier/svm.py
@@ -1,8 +1,12 @@
 # https://github.com/scikit-learn/scikit-learn/issues/19137
+# https://joyfuls.tistory.com/60
+# https://blog.naver.com/PostView.naver?blogId=racoonpapa&logNo=222435398541&redirect=Dlog&widgetTypeCall=true&directAccess=false
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC  # model 생성
 from sklearn.model_selection import train_test_split  # train/test set
 from sklearn.metrics import accuracy_score, confusion_matrix  # model 평가
+from collections import Counter
 
 
 def run():
@@ -10,15 +14,21 @@ def run():
     y = np.load("dataset_y.npy", allow_pickle=True)
     print(X.shape)
     print(y.shape)
+    y = np.array(list(map(str, y)))
+    print(Counter(y))
+    print(y)
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(X)
+
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
 
-    print(x_train.shape)  # (105, 4)
-    print(y_train.shape)  # (105,)
+    print(x_train.shape)  # (361, 6373)
+    print(y_train.shape)  # (361,)
 
     # 3. model
-    help(SVC)
-    model = SVC()  # kernel = 'rbf'  : default
+    model = SVC(kernel='linear', verbose=1)  # kernel = 'rbf'  : default
     # 데이터의 특징 때문에 분류정확도가 낮게 나오면 다른 커널 함수를 인수로 지정할 수 있음
+    # 'linear' is too slow; others are bad
     # 'linear', 'poly', 'rbf', 'sigmoid' 등
 
     model.fit(X=x_train, y=y_train)
@@ -29,26 +39,25 @@ def run():
 
     # 분류정확도 - accuracy_score 함수 사용해서 구하기
     acc = accuracy_score(y_true, y_pred)
-    acc  # 0.9777777777777777
+    print(acc)  # 0.3853211009174312
 
     con_mat = confusion_matrix(y_true, y_pred)
-    con_mat
+    print(con_mat)
     '''
-    array([[18,  0,  0],
-           [ 0, 10,  0],
-           [ 0,  1, 16]], dtype=int64)
+    [[ 2  4  0 17]
+    [ 1  9  1 20]
+    [ 1  4  0 23]
+    [ 2  3  0 22]]
     '''
-    y_true[:10]  # [1, 2, 2, 1, 0, 2, 1, 0, 0, 1]
-    y_pred[:10]  # [1, 2, 2, 1, 0, 1, 1, 0, 0, 1]
+    print(y_true[:10])  # [3 3 2 3 3 2 2 2 4 4]
+    print(y_pred[:10])  # [4 4 4 4 4 4 4 4 4 4]
 
-    type(con_mat)  # numpy.ndarray
-    con_mat.shape  # (3, 3)
+    print(type(con_mat))  # numpy.ndarray
+    print(con_mat.shape)  # (4, 4)
 
     # 분류정확도 - 식을 통해 구하기
     acc = (con_mat[0, 0] + con_mat[1, 1] + con_mat[2, 2]) / len(y_true)
-    acc  # 0.9777777777777777
-
-    ## iris 데이터의 경우, NB모델보다 SVM모델을 사용할 때 분류정확도가 더 올라간 것을 확인할 수 있음
+    print(acc)  # 0.25688073394495414
 
 
 if __name__ == '__main__':
